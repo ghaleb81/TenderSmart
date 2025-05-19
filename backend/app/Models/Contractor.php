4b99f3030d925430e70efcfd8fc0661f7c5aca99
EyadO23 <alkhatibeyad3@gmail.com>
2025-05-20 02:53:18 +0300

    Add backend folder with Laravel code

---
+++ backend/app/Models/Contractor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contractor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'company_name',
+        'commercial_registration_number',
+        'company_email',
+        'country',
+        'city',
+        'phone_number',
+        'year_established',
+        'projects_last_5_years',
+        'quality_certificates',
+        'public_sector_successful_contracts',
+        'website_url',
+        'linkedin_profile',
+        'company_bio',
+        'official_documents',
+    ];
+
+    protected $casts = [
+        'quality_certificates' => 'array', // تحويلها تلقائياً لمصفوفة عند القراءة والكتابة
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function bids()
+{
+    return $this->hasMany(Bid::class);
+}
+
+}
